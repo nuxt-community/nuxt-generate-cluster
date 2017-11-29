4e07d5d073c737907d0282e62ce9cb8ea5b54084
pimlie <pimlie@hotmail.com>
2017-11-29 22:50:49 +0100

    add cli integration test

---
+++ test/cli.test.js
@@ -0,0 +1,22 @@
+import test from 'ava'
+import { resolve, sep } from 'path'
+import pify from 'pify'
+import { exec } from 'child_process'
+
+const execify = pify(exec, { multiArgs: true })
+const rootDir = resolve(__dirname, 'fixtures/basic')
+
+test('bin/nuxt-generate', async t => {
+  const binGenerate = resolve(__dirname, '..', 'bin', 'nuxt-generate')
+
+  const [ stdout, stderr ] = await execify(`node ${binGenerate} -b ${rootDir} -w 2`)
+
+  t.true(stdout.includes('server-bundle.json'))
+  t.true(stderr.includes('Call ready hooks'))
+  t.true(stderr.includes('Worker 1 started'))
+  t.true(stderr.includes('Worker 2 started'))
+  t.true(stderr.includes('generated file'))
+  t.true(stderr.includes(`${sep}users${sep}1${sep}index.html`))
+  t.true(stderr.includes('Worker 1 exited'))
+  t.true(stderr.includes('Worker 2 exited'))
+})
