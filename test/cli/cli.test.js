c0ed279e303af3f5dc7d4850f6feae9ad3416a6c
Reto Kaiser <reto@retoonline.com>
2019-01-26 14:58:01 +0100

    Make sure script exits with code 0 on errors

--- test/cli/cli.test.js
+++ test/cli/cli.test.js
@@ -1,22 +1,8 @@
 import path from 'path'
 import { execFile } from 'promisify-child-process'
+import 'jest-extended'
 
 describe('cli', () => {
-  test('bin/nuxt-generate', async () => {
-    const result = await runGenerate('basic')
-
-    expect(result.stderr).toContain('==== Error report ====')
-    expect(result.stdout).toContain('Nuxt files generated')
-    expect(result.stdout).toContain('worker 1 started')
-    expect(result.stdout).toContain('worker 2 started')
-    expect(result.stdout).not.toContain('worker 3 started')
-    expect(result.stdout).toContain('generated: ')
-    expect(result.stdout).toContain(`${path.sep}users${path.sep}1${path.sep}index.html`)
-    expect(result.stdout).toContain('worker 1 exited')
-    expect(result.stdout).toContain('worker 2 exited')
-    expect(result.stdout).toContain('HTML Files generated in')
-  })
-
   test('bin/nuxt-generate no error', async () => {
     const result = await runGenerate('no-error')
 
@@ -28,6 +14,39 @@ describe('cli', () => {
     expect(result.stdout).toContain('HTML Files generated in')
   })
 
+  test('bin/nuxt-generate with unhandled rendering error', async () => {
+    await expect(runGenerate('unhandled-error')).rejects.toSatisfy((result) => {
+      expect(result.code).toEqual(1)
+      expect(result.stderr).toContain('There were 1 unhandled page rendering errors.')
+      return true
+    })
+  })
+
+  test('bin/nuxt-generate with killed worker', async () => {
+    await expect(runGenerate('kill-error')).rejects.toSatisfy((result) => {
+      expect(result.code).toEqual(1)
+      expect(result.stderr).toContain('1 workers failed')
+      return true
+    })
+  })
+
+  test('bin/nuxt-generate with a complex site', async () => {
+    await expect(runGenerate('basic')).rejects.toSatisfy((result) => {
+      expect(result.code).toEqual(1)
+      expect(result.stdout).toContain('Nuxt files generated')
+      expect(result.stdout).toContain('worker 1 started')
+      expect(result.stdout).toContain('worker 2 started')
+      expect(result.stdout).not.toContain('worker 3 started')
+      expect(result.stdout).toContain('generated: ')
+      expect(result.stdout).toContain(`${path.sep}users${path.sep}1${path.sep}index.html`)
+      expect(result.stdout).toContain('worker 1 exited')
+      expect(result.stdout).toContain('worker 2 exited')
+      expect(result.stdout).toContain('HTML Files generated in')
+      expect(result.stderr).toContain('==== Error report ====')
+      expect(result.stderr).toContain('There were 3 unhandled page rendering errors.')
+      return true
+    })
+  })
 })
 
 /**
---
+++ test/fixtures/kill-error/pages/my_page.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    process.kill(process.pid, 'SIGABRT')
+  }
+}
+</script>
