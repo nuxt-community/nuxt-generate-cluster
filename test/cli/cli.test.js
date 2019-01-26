9964f560ed3a8b06a53647a949151158df45e387
Reto Kaiser <reto@retoonline.com>
2019-01-26 13:32:45 +0100

    Extract function to run 'nuxt-generate' script in tests

--- test/cli/cli.test.js
+++ test/cli/cli.test.js
@@ -1,44 +1,42 @@
 import path from 'path'
-import spawn from 'cross-spawn'
-
-const rootDir = path.resolve(__dirname, '..', 'fixtures', 'basic')
+import { execFile } from 'promisify-child-process'
 
 describe('cli', () => {
   test('bin/nuxt-generate', async () => {
-    const { stdout, stderr, error } = await new Promise((resolve) => {
-      let stdout = ''
-      let stderr = ''
-      let error
-      const env = {}
-      env.NODE_ENV = 'production'
+    const result = await runGenerate('nuxt.config.js')
 
-      // Nuxt sets log level to 0 for CI and env=TEST
-      // -v offsets from default log level, not current level
-      // hence one -v is enough
-      const binGenerate = path.resolve(__dirname, '..', '..', 'bin', 'nuxt-generate')
-
-      const nuxtGenerate = spawn(binGenerate, [
-        '-b', rootDir, '-w', '2', '-v'
-      ], { env })
-
-      nuxtGenerate.stdout.on('data', (data) => { stdout += data.toString() })
-      nuxtGenerate.stderr.on('data', (data) => { stderr += data.toString() })
-      nuxtGenerate.on('error', (err) => { error = err })
-      nuxtGenerate.on('close', () => {
-        resolve({ stdout, stderr, error })
-      })
-    })
-
-    expect(error).toBeUndefined()
-    expect(stderr).toContain('==== Error report ====')
-    expect(stdout).toContain('Nuxt files generated')
-    expect(stdout).toContain('worker 1 started')
-    expect(stdout).toContain('worker 2 started')
-    expect(stdout).not.toContain('worker 3 started')
-    expect(stdout).toContain('generated: ')
-    expect(stdout).toContain(`${path.sep}users${path.sep}1${path.sep}index.html`)
-    expect(stdout).toContain('worker 1 exited')
-    expect(stdout).toContain('worker 2 exited')
-    expect(stdout).toContain('HTML Files generated in')
+    expect(result.stderr).toContain('==== Error report ====')
+    expect(result.stdout).toContain('Nuxt files generated')
+    expect(result.stdout).toContain('worker 1 started')
+    expect(result.stdout).toContain('worker 2 started')
+    expect(result.stdout).not.toContain('worker 3 started')
+    expect(result.stdout).toContain('generated: ')
+    expect(result.stdout).toContain(`${path.sep}users${path.sep}1${path.sep}index.html`)
+    expect(result.stdout).toContain('worker 1 exited')
+    expect(result.stdout).toContain('worker 2 exited')
+    expect(result.stdout).toContain('HTML Files generated in')
   })
 })
+
+/**
+ * @param {String} configFile
+ * @returns {Promise<{stdout: string, stderr: string}>}
+ */
+function runGenerate(configFile) {
+  const rootDir = path.resolve(__dirname, '..', 'fixtures', 'basic')
+  // Nuxt sets log level to 0 for CI and env=TEST
+  // -v offsets from default log level, not current level
+  // hence one -v is enough
+  const args = [
+    rootDir,
+    '--build',
+    '--workers=2',
+    `--config-file=${configFile}`,
+    '-v'
+  ]
+  const env = {
+    NODE_ENV: 'production'
+  }
+  const binGenerate = path.resolve(__dirname, '..', '..', 'bin', 'nuxt-generate')
+  return execFile(binGenerate, args, { env })
+}
